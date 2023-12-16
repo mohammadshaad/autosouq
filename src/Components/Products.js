9be7205cadc136feacd411d821cd112e5bfc27d6
Mohammad Shaad <mohammadshaadshaikh@gmail.com>
2023-12-16 20:30:53 +0530

    Update products wrapper with padding

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -191,7 +191,7 @@ export const Products = () => {
 
   return (
     <div className="products-main-container">
-      <div className="products-wrapper">
+      <div className="products-wrapper py-20">
         <div className="flex md:flex-row flex-col gap-10 md:gap-0 items-center md:items-start justify-center md:justify-between w-full">
           <div className=" md:w-1/3 ">
             <div className="bg-white shadow-lg rounded-full md:rounded-3xl">
