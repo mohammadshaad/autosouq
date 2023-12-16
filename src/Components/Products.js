1f3cc7d52a44397315d3f7567ff775e12f58a7bc
Mohammad Shaad <mohammadshaadshaikh@gmail.com>
2023-12-17 00:04:44 +0530

    Remove unused code for product rating

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -68,17 +68,6 @@ const filters = [
     ],
   },
   {
-    id: "ProductRating",
-    name: "Rating",
-    options: [
-      { value: "1", label: "1", checked: false },
-      { value: "2", label: "2", checked: false },
-      { value: "3", label: "3", checked: false },
-      { value: "4", label: "4", checked: false },
-      { value: "5", label: "5", checked: false },
-    ],
-  },
-  {
     id: "regionalSpecs",
     name: "Regional Specs",
     options: [
@@ -117,52 +106,6 @@ const filters = [
       { value: "No", label: "No", checked: false },
     ],
   },
-  // {
-  //   id: "productPrice",
-  //   name: "Product Price",
-  //   options: [
-  //     { value: "10000", label: "10000", checked: false },
-  //     { value: "20000", label: "20000", checked: false },
-  //     { value: "30000", label: "30000", checked: false },
-  //     { value: "40000", label: "40000", checked: false },
-  //     { value: "50000", label: "50000", checked: false },
-  //   ],
-  // },
-  // {
-  //   id: "color",
-  //   name: "Color",
-  //   options: [
-  //     { value: "white", label: "White", checked: false },
-  //     { value: "beige", label: "Beige", checked: false },
-  //     { value: "blue", label: "Blue", checked: true },
-  //     { value: "brown", label: "Brown", checked: false },
-  //     { value: "green", label: "Green", checked: false },
-  //     { value: "purple", label: "Purple", checked: false },
-  //   ],
-  // },
-  // {
-  //   id: "category",
-  //   name: "Category",
-  //   options: [
-  //     { value: "new-arrivals", label: "New Arrivals", checked: false },
-  //     { value: "sale", label: "Sale", checked: false },
-  //     { value: "travel", label: "Travel", checked: true },
-  //     { value: "organization", label: "Organization", checked: false },
-  //     { value: "accessories", label: "Accessories", checked: false },
-  //   ],
-  // },
-  // {
-  //   id: "size",
-  //   name: "Size",
-  //   options: [
-  //     { value: "2l", label: "2L", checked: false },
-  //     { value: "6l", label: "6L", checked: false },
-  //     { value: "12l", label: "12L", checked: false },
-  //     { value: "18l", label: "18L", checked: false },
-  //     { value: "20l", label: "20L", checked: false },
-  //     { value: "40l", label: "40L", checked: true },
-  //   ],
-  // },
 ];
 
 function classNames(...classes) {
@@ -690,41 +633,6 @@ export const Products = () => {
                             {product.ProductCity}
                           </div>
                         </div>
-
-                        {/* <div className="flex items-center justify-center gap-1">
-                          <div>
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              strokeWidth={1.5}
-                              stroke="currentColor"
-                              className="w-4 text-black"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
-                              />
-                            </svg>
-                          </div>
-
-                          <div>
-                            {product.ProductRating === 0 ? (
-                              <div className="flex items-center justify-center gap-1">
-                                <div className="font-medium">No</div>
-                                <div className="font-medium">Ratings</div>
-                              </div>
-                            ) : (
-                              <div className="flex items-center justify-center gap-1">
-                                <div className="font-medium">
-                                  {product.ProductRating}
-                                </div>
-                                <div className="font-medium">Ratings</div>
-                              </div>
-                            )}
-                          </div>
-                        </div> */}
                       </div>
                     </div>
 
