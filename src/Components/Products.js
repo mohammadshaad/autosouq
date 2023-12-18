67e740db1483a10294a4a4d0f9da1d7ba266748c
Mohammad Shaad <mohammadshaadshaikh@gmail.com>
2023-12-18 22:43:37 +0530

    Update landing page header styles and text

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -216,7 +216,7 @@ export const Products = () => {
   };
 
   return (
-    <div className="products-main-container z-50">
+    <div className="products-main-container !z-50">
       <div className="z-50 products-wrapper py-20">
         <div className="z-50 flex md:flex-row flex-col gap-10 md:gap-0 items-start md:items-start justify-center md:justify-between w-full">
           <div className="z-50 w-full px-14 lg:px-0 md:w-1/3 ">
@@ -252,7 +252,7 @@ export const Products = () => {
                         leaveTo="translate-x-full"
                       >
                         <Dialog.Panel className="!z-50 relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-gray-900 py-4 pb-12 shadow-xl">
-                          <div className="flex items-center justify-between px-4">
+                          <div className="flex items-center justify-between px-4 z-50">
                             <h2 className="text-lg font-medium text-gray-200">
                               Filters
                             </h2>
