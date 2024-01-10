87918f506e9ea243b6f1324ab325f45f253d3846
Mohammad Shaad <mohammadshaadshaikh@gmail.com>
2024-01-10 16:27:27 +0530

    Update user authentication logic to use bcrypt for password hashing

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -18,16 +18,15 @@ export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(true);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [role, setRole] = useState(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
   useEffect(() => {
-    // Set up an observer on the Auth object
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
-        // User is signed in.
         db.collection("SignedUpUsersData")
           .doc(user.uid)
           .get()
@@ -52,18 +51,15 @@ export const Navbar = () => {
             }
           });
       } else {
-        // No user is signed in.
         setCurrentUser(null);
         setProfileImageUrl(null);
         setRole(null);
       }
     });
 
-    // Clean up the observer when the component unmounts
     return () => unsubscribe();
   }, [totalQty]);
 
-  // handle logout
   const handleLogout = () => {
     auth.signOut().then(() => {
       history.push("/");
@@ -74,15 +70,19 @@ export const Navbar = () => {
     setIsOpen(!isOpen);
   };
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div className="navbox shadow-xl z-50">
-      {/* <div className="mobile-nav">
+    <div className="navbox shadow-xl z-50 ">
+      <div className="mobile-nav ">
         <Link to="/" className="logo">
           <img src={logo} alt="AutoSouq Logo" />
         </Link>
 
-        <div className="menu-buttons" onClick={handleMenu}>
-          {!isOpen && (
+        <div className="menu-buttons z-50" onClick={toggleMenu}>
+          {!isOpen ? (
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -94,9 +94,7 @@ export const Navbar = () => {
               <rect x="2" y="11" width="18" height="2" />
               <rect x="2" y="17" width="16" height="2" />
             </svg>
-          )}
-
-          {isOpen && (
+          ) : (
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -108,184 +106,136 @@ export const Navbar = () => {
             </svg>
           )}
         </div>
-      </div> */}
+      </div>
 
-      <div className="flex items-center justify-center w-full">
-        <div className="desktop-nav-menu">
+      <div className="flex items-center justify-center w-full md:p-4 ">
+        <div className="desktop-nav-menu hidden md:flex items-center justify-center ">
           <Link to="/" className="logo">
             <img src={logo} alt="AutoSouq Logo" />
           </Link>
         </div>
 
-        {currentUser && (
-          <div className="relative rightside no-underline  ">
-            {/* <Link to="/profile" className="group flex items-center justify-center flex-row  flex-shrink-0 gap-2 no-underline underline-offset-4">
-            <Link to="/profile" className="group-hover:opacity-60 duration-200 transition-all navlink current-user text-xs no-underline appearance-none text-black">
-              {currentUser}
-            </Link>
-            <span className="">
-              <Icon icon={user} className="group-hover:opacity-60 text-black duration-200 transition-all user" size={23} />
-            </span>
-          </Link> */}
-
-            <div className=" flex items-center justify-end w-full">
-              <button
-                type="button"
-                className="inline-flex items-center justify-center p-2 rounded-full text-white hover:text-gray-300 focus:outline-none "
-                onClick={toggleDropdown}
-              >
-                {profileImageUrl ? (
-                  <div className="p-2 rounded-3xl border border-gray-400 flex items-center justify-center gap-1">
-                    <img
-                      src={profileImageUrl}
-                      alt="Profile"
-                      className="h-12 w-12 rounded-full"
-                    />
-                    <div>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={2}
-                        stroke="currentColor"
-                        className={`${
-                          isDropdownOpen ? "-rotate-180" : "rotate-0"
-                        } duration-200 transition-all w-4 h-4 text-gray-500`}
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                        />
-                      </svg>
-                    </div>
-                  </div>
-                ) : (
-                  <div className=" shadow-lg border border-lightOrange text-gray-600 hover:bg-lightOrange hover:border-lightOrange hover:text-white transition-all duration-200 rounded-full p-2">
+        {/* {currentUser && (
+          <div className="relative rightside no-underline bg-red-400">
+            <button
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-full text-white hover:text-gray-300 focus:outline-none"
+              onClick={toggleDropdown}
+            >
+              {profileImageUrl ? (
+                <div className="p-2 rounded-3xl border border-gray-400 flex items-center justify-center gap-1">
+                  <img
+                    src={profileImageUrl}
+                    alt="Profile"
+                    className="h-12 w-12 rounded-full"
+                  />
+                  <div>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
-                      strokeWidth={1}
+                      strokeWidth={2}
                       stroke="currentColor"
-                      className="w-6 h-6 md:w-8 md:h-8 "
+                      className={`${
+                        isDropdownOpen ? "-rotate-180" : "rotate-0"
+                      } duration-200 transition-all w-4 h-4 text-gray-500`}
                     >
                       <path
                         strokeLinecap="round"
                         strokeLinejoin="round"
-                        d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
                       />
                     </svg>
                   </div>
-                )}
-              </button>
-
-              {/* Dropdown */}
-              {isDropdownOpen && (
-                <div className="origin-top-right absolute top-14 right-0 mt-2 w-48 z-50 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-                  <div
-                    className="py-1 z-50"
-                    role="menu"
-                    aria-orientation="vertical"
-                    aria-labelledby="options-menu"
+                </div>
+              ) : (
+                <div className=" shadow-lg border border-lightOrange text-gray-600 hover:bg-lightOrange hover:border-lightOrange hover:text-white transition-all duration-200 rounded-full p-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1}
+                    stroke="currentColor"
+                    className="w-6 h-6 md:w-8 md:h-8 "
                   >
-                    {/* Dropdown items */}
-                    <a
-                      href="/profile"
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                    />
+                  </svg>
+                </div>
+              )}
+            </button>
+
+            {isDropdownOpen && (
+              <div className="origin-top-right absolute top-20 md:top-14 md:right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                <div
+                  className="py-1"
+                  role="menu"
+                  aria-orientation="vertical"
+                  aria-labelledby="options-menu"
+                >
+                  <a
+                    href="/profile"
+                    className="block appearance-none hover:no-underline px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-lightOrange"
+                    role="menuitem"
+                  >
+                    Profile
+                  </a>
+                  {role === "dealer" ? (
+                    <Link
+                      to="/dashboard"
                       className="block appearance-none hover:no-underline px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-lightOrange"
                       role="menuitem"
                     >
-                      Profile
-                    </a>
-                    {/* <Link
+                      Dashboard
+                    </Link>
+                  ) : (
+                    <Link
                       to="/wishlist"
                       className="block appearance-none hover:no-underline px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-lightOrange"
                       role="menuitem"
                     >
                       Wishlist
-                    </Link> */}
-                    {role === "dealer" ? (
-                      <Link
-                        to="/dashboard"
-                        className=" block appearance-none hover:no-underline px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-lightOrange"
-                        role="menuitem"
-                      >
-                        Dashboard
-                      </Link>
-                    ) : (
-                      <Link
-                        to="/wishlist"
-                        className="block appearance-none hover:no-underline px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-lightOrange"
-                        role="menuitem"
-                      >
-                        Wishlist
-                      </Link>
-                    )}
-                    <a
-                      href="#"
-                      className="block appearance-none hover:no-underline px-4 py-2 text-sm text-gray-700 hover:bg-gray-100  hover:text-lightOrange"
-                      role="menuitem"
-                      onClick={handleLogout}
-                    >
-                      Log out
-                    </a>
-                  </div>
+                    </Link>
+                  )}
+                  <a
+                    href="#"
+                    className="block appearance-none hover:no-underline px-4 py-2 text-sm text-gray-700 hover:bg-gray-100  hover:text-lightOrange"
+                    role="menuitem"
+                    onClick={handleLogout}
+                  >
+                    Log out
+                  </a>
                 </div>
-              )}
-            </div>
-
-            {/* <span>
-            <Link to="cart" className="navlink">
-              <Icon icon={cart} className="cart" />
-              <span className="no-of-products">{totalQty}</span>
-            </Link>
-          </span> */}
-            {/* <span className="flex items-center justify-center ml-3">
-            <Link to="cart" className="navlink cart-icon absolute">
-              <Icon icon={cart} className="cart" size={20}/>
-              <span className="no-of-products relative bottom-2 px-1">
-                {totalQty}
-              </span>
-            </Link>
-          </span> */}
-            {/* <span>
-            <button className="login" onClick={handleLogout}>
-              Logout
-            </button>
-          </span> */}
+              </div>
+            )}
           </div>
-        )}
+        )} */}
+
         {!currentUser && isOpen && (
-          <div className="p-4 flex items-center gap-4 md:gap-2 justify-end w-full">
-            {/* <span className="flex items-center justify-center ">
-            <Link to="cart" className="navlink cart-icon absolute">
-              <Icon icon={cart} className="cart" />
-              <span className="no-of-products relative bottom-2 px-1">
-                {totalQty}
-              </span>
-            </Link>
-          </span> */}
-            {/* <span>
-            <Link to="login" className="navlink login">
-              LOGIN
-            </Link>
-          </span> */}
-            <span className="">
-              <Link
-                to="login"
-                className="navlink sign-up !text-[10px] !px-4 !py-3 md:!text-sm"
-              >
-                LOGIN
-              </Link>
-            </span>
-            <span className="">
-              <Link
-                to="login"
-                className="navlink sign-up !text-[10px] !px-4 !py-3 md:!text-sm"
-              >
-                LOGIN AS DEALER
-              </Link>
-            </span>
+          <div className="flex items-center gap-4 md:gap-2 justify-end w-full ">
+            {(isMenuOpen || window.innerWidth > 768) && (
+              <div className="flex items-start justify-center md:justify-end md:px-4 flex-col md:flex-row gap-6  w-full ">
+                <span className="">
+                  <Link
+                    to="login"
+                    className="navlink sign-up !text-[10px] !px-4 !py-3 md:!text-sm"
+                  >
+                    LOGIN
+                  </Link>
+                </span>
+                <span className="flex-shrink-0">
+                  <Link
+                    to="login"
+                    className="navlink sign-up !text-[10px] !px-4 !py-3 md:!text-sm flex-shrink-0"
+                  >
+                    LOGIN AS DEALER
+                  </Link>
+                </span>
+              </div>
+            )}
           </div>
         )}
       </div>
